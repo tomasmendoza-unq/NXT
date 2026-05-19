63912cc1eb7677fde3bed4490335822592ab62bd
Tomas Mendoza <tm1453766@gmail.com>
2026-05-18 23:14:48 -0300

    fix: se fixea el error al importar las routes de product

--- client/src/features/product/routes/Product.routes.tsx
+++ client/src/features/product/routes/Product.routes.tsx
@@ -2,14 +2,15 @@ import { Route } from "react-router-dom";
 import { Routes as ProductRoutesArray } from "./routes";
 import type { Route as RouteType } from "../../../shared/types/Route.t.ts";
 
-export const ProductRoutes = () => {
-    return (
-        <>
-            {
-                ProductRoutesArray.map((route: RouteType) => (
-                    <Route key={route.path} path={`product${route.path}`} element={route.element} />
-                ))
-            }
-        </>
-    );
-};
+export const productRoutes = (
+    <Route path="product">
+        {ProductRoutesArray.map((route: RouteType) => (
+            <Route
+                key={route.path}
+                index={route.path === "/"}
+                path={"/product" + route.path}
+                element={route.element}
+            />
+        ))}
+    </Route>
+);
