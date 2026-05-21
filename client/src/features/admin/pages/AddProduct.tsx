c22cba62bfd8119742244d942eaa74edbd6950dd
MunneLautaro <lautaro_munne@outlook.com>
2026-05-21 15:47:28 -0300

    Se agregan los componetes para el formulario de AgregarProducto

--- client/src/features/admin/pages/AddProduct.tsx
+++ client/src/features/admin/pages/AddProduct.tsx
@@ -1,7 +1,10 @@
+import { ProductForm } from "./components/ProductForm";
+
 export const AddProduct = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1>Add Product</h1>
+    <div className="flex flex-col justify-center items-center gap-6">
+      <h1>Add Products</h1>
+      <ProductForm />
     </div>
   );
 };
