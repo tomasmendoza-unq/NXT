87a2ca08f3be8b22e07aef78d31dbaaf132e2d8c
Tomas Mendoza <tm1453766@gmail.com>
2026-06-04 21:22:18 -0300

    feature: se agrega la opcion de que los productos tengan fotos secundarias

--- server/src/main/java/com/tm/nc/features/product/controller/dto/ProductDetailsResponseDTO.java
+++ server/src/main/java/com/tm/nc/features/product/controller/dto/ProductDetailsResponseDTO.java
@@ -2,12 +2,15 @@ package com.tm.nc.features.product.controller.dto;
 
 import com.tm.nc.domain.product.model.ProductDetail;
 
+import java.util.List;
+
 public record ProductDetailsResponseDTO(
         Long id,
         int size,
         double price,
         String image,
-        int quantity
+        int quantity,
+        List<String> gallery
 ) {
     public static ProductDetailsResponseDTO fromDetails(ProductDetail details) {
         return new ProductDetailsResponseDTO(
@@ -15,7 +18,8 @@ public record ProductDetailsResponseDTO(
                 details.getSize(),
                 details.getPrice(),
                 details.getImage(),
-                details.getQuantity()
+                details.getQuantity(),
+                details.getGallery()
         );
     }
 }
