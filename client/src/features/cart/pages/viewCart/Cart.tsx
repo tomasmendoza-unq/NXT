e5500b64167c8fa5c123075b01f0575b77a6f67d
Tomas Mendoza <tm1453766@gmail.com>
2026-06-07 12:54:22 -0300

    style: se acomoda el cart-items para que se active el scroll del content y no da la pagina

--- client/src/features/cart/pages/viewCart/Cart.tsx
+++ client/src/features/cart/pages/viewCart/Cart.tsx
@@ -16,18 +16,33 @@ export const Cart = () => {
     return items.length === 0 ? (
         <p>Carrito está vacío</p>
     ) : (
-        <div>
-            <h1>Carrito</h1>
-            <ul className="cart-items">
-                {items.map((item, index) => (
-                    <li key={item.product.id}>
-                        <ItemCard item={item} />
-                        {index < items.length - 1 && (
-                            <hr className="cart-divider" />
-                        )}
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <main className="cart-container">
+            <section className="cart-content">
+                <h1>Carrito</h1>
+
+                <ul className="cart-items">
+                    {items.map((item, index) => (
+                        <li key={item.product.id}>
+                            <ItemCard item={item} />
+                            {index < items.length - 1 && (
+                                <hr className="cart-divider" />
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            </section>
+            <aside className="cart-summary">
+                <h2>Resumen del carrito</h2>
+                <p>Total de artículos: {items.length}</p>
+                <p>
+                    Total a pagar: $
+                    {items.reduce(
+                        (total, item) =>
+                            total + item.detail.price * item.quantity,
+                        0,
+                    )}
+                </p>
+            </aside>
+        </main>
     );
 };
