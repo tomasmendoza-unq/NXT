349186bfedbf0ebf9f6bfdc10f5322d47e3769ac
Tomas Mendoza <tm1453766@gmail.com>
2026-06-08 11:42:19 -0300

    style: se agrupan los inputs para que entren en tamaño desktop

--- client/src/features/checkout/pages/components/formFacturation/inputs.ts
+++ client/src/features/checkout/pages/components/formFacturation/inputs.ts
@@ -1,45 +1,53 @@
 export const inputs = [
-    {
-        name: "firstName",
-        label: "Nombre",
-        type: "text",
-    },
-    {
-        name: "lastName",
-        label: "Apellido",
-        type: "text",
-    },
-    {
-        name: "address",
-        label: "Dirección",
-        type: "text",
-    },
-    {
-        name: "province",
-        label: "Provincia",
-        type: "text",
-    },
-    {
-        name: "city",
-        label: "Localidad",
-        type: "text",
-    },
-    {
-        name: "postalCode",
-        label: "Código Postal",
-        type: "text",
-    },
-    {
-        name: "phone",
-        label: "Teléfono",
-        type: "tel",
-        pattern: "[0-9]{10}",
-        title: "Debe cumplir con el formato de un teléfono",
-    },
-    {
-        name: "email",
-        label: "Correo Electrónico",
-        type: "email",
-        title: "Debe cumplir con el formato de un email",
-    },
+    [
+        {
+            name: "firstName",
+            label: "Nombre",
+            type: "text",
+        },
+        {
+            name: "lastName",
+            label: "Apellido",
+            type: "text",
+        },
+    ],
+    [
+        {
+            name: "address",
+            label: "Dirección",
+            type: "text",
+        },
+    ],
+    [
+        {
+            name: "province",
+            label: "Provincia",
+            type: "text",
+        },
+        {
+            name: "city",
+            label: "Localidad",
+            type: "text",
+        },
+        {
+            name: "postalCode",
+            label: "Código Postal",
+            type: "text",
+        },
+    ],
+    [
+        {
+            name: "phone",
+            label: "Teléfono",
+            type: "tel",
+            pattern: "[0-9]{10}",
+            title: "Debe cumplir con el formato de un teléfono",
+        },
+        {
+            name: "email",
+            label: "Correo Electrónico",
+            type: "email",
+            title: "Debe cumplir con el formato de un email",
+        },
+    ],
 ];
