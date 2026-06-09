025927b9aa2c6a8154c352a2177509a8e976306f
Tomas Mendoza <tm1453766@gmail.com>
2026-06-09 15:10:04 -0300

    feature: se agregan validaciones de schemas en el backend para las request y se muestran en el front con un toast

--- server/src/main/java/com/tm/nc/features/cart/controller/dto/request/CartRequestDTO.java
+++ server/src/main/java/com/tm/nc/features/cart/controller/dto/request/CartRequestDTO.java
@@ -1,8 +1,16 @@
 package com.tm.nc.features.cart.controller.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public record CartRequestDTO(
+    @NotNull(message = "El id del color es requerido")
     Long idColor,
+
+    @NotNull(message = "El id del detalle es requerido")
     Long idDetails,
+
+    @Min(value = 1, message = "La cantidad debe ser mayor a 0")
     int quantity
 ) {
 
--- server/src/main/java/com/tm/nc/features/cart/controller/dto/request/CheckRequestDTO.java
+++ server/src/main/java/com/tm/nc/features/cart/controller/dto/request/CheckRequestDTO.java
@@ -1,7 +1,12 @@
 package com.tm.nc.features.cart.controller.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public record CheckRequestDTO(
+        @NotNull(message = "El id del detalle es requerido")
         Long detailId,
+        @Min(value = 1, message = "La cantidad debe ser mayor a 0")
         Integer quantity
 ) {
 }
