b10a4b1e753b9d344a7186ccbab26046ee20b499
Tomas Mendoza <tm1453766@gmail.com>
2026-06-10 11:45:39 -0300

    feature: se implementa el login para usuarios

--- client/src/features/auth/pages/login/Login.tsx
+++ client/src/features/auth/pages/login/Login.tsx
@@ -1,19 +1,18 @@
+import { useLogin } from "../../hooks/use-login";
 import { LoginForm } from "./components/LoginForm";
 import "./style/Login.css";
 
 export const Login = () => {
-    const formData = {
-        email: "",
-        password: "",
-    };
+    const { formData, isLoading, handleChange, handleSubmit } = useLogin();
 
     return (
         <section className="login-container">
             <h1>Bienvenido</h1>
             <LoginForm
                 formData={formData}
-                onChange={() => {}}
-                onSubmit={() => {}}
+                isLoading={isLoading}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
             />
         </section>
     );
