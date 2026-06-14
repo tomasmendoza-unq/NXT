b07ba5ebcd6cc2d9d9e04b6eab8a23eea198d625
Tomas Mendoza <tm1453766@gmail.com>
2026-06-14 17:57:39 -0300

    feature: se implementa un endpoint para obtener las ordenes de un cliente logueado

--- server/src/main/java/com/tm/nc/domain/checkout/persistence/sql/CheckoutDAOSQL.java
+++ server/src/main/java/com/tm/nc/domain/checkout/persistence/sql/CheckoutDAOSQL.java
@@ -2,6 +2,7 @@ package com.tm.nc.domain.checkout.persistence.sql;
 
 import com.tm.nc.domain.checkout.model.Checkout;
 import com.tm.nc.domain.checkout.model.enums.CheckoutStatus;
+import com.tm.nc.domain.client.model.Client;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,8 @@ public interface CheckoutDAOSQL extends JpaRepository<Checkout, Long> {
     Page<Checkout> findAllByStatus(CheckoutStatus status, Pageable pageable);
 
     List<Checkout> findAllByStatus(CheckoutStatus status);
+
+    Page<Checkout> findAllByStatusAndClient(CheckoutStatus status, Client client,  Pageable pageable);
+
+    Page<Checkout> findAllByClient(Client client, Pageable pageable);
 }
