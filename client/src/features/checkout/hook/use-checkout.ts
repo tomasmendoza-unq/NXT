0dafb1dec8e7a15d2ff080b19af79f13074e2b69
Tomas Mendoza <tm1453766@gmail.com>
2026-06-17 16:29:49 -0300

    feature: ahora si el usuario esta logueado cuando va a comprar, se envia la peticion a client checkout

--- client/src/features/checkout/hook/use-checkout.ts
+++ client/src/features/checkout/hook/use-checkout.ts
@@ -6,6 +6,8 @@ import { postCheckout } from "../service/post-checkout.service";
 import { checkoutToRequestDTO } from "../adapter/checkout.adapter";
 import type { Checkout } from "../types/checkout.t";
 import type { CheckoutResponseDTO } from "../api/types/CheckoutResponseDTO";
+import { useAuth } from "../../auth/hooks/use-auth";
+import { postCheckoutClient } from "../../client/service/post-checkout-client.service";
 
 export const useCheckout = () => {
     const [checkoutData, setCheckoutData] =
@@ -14,24 +16,39 @@ export const useCheckout = () => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [showSuccessMessage, setShowSuccessMessage] =
         useState<boolean>(false);
+    const { isAuthenticated } = useAuth();
 
     const checkout = async (formData: FacturationForm) => {
         setIsLoading(true);
-        try {
-            const checkout: Checkout = {
-                facturationForm: formData,
-                items: localCart.map((item) => ({
-                    idDetail: item.detailId,
-                    quantity: item.quantity,
-                })),
-            };
-
-            const response = await postCheckout(checkoutToRequestDTO(checkout));
-            setCheckoutData(response);
-            cartService.clearCart();
-            setShowSuccessMessage(true);
-        } finally {
-            setIsLoading(false);
+        const checkout: Checkout = {
+            facturationForm: formData,
+            items: localCart.map((item) => ({
+                idDetail: item.detailId,
+                quantity: item.quantity,
+            })),
+        };
+        if (isAuthenticated) {
+            try {
+                const response = await postCheckoutClient(
+                    checkoutToRequestDTO(checkout),
+                );
+                setCheckoutData(response);
+                cartService.clearCart();
+                setShowSuccessMessage(true);
+            } finally {
+                setIsLoading(false);
+            }
+        } else {
+            try {
+                const response = await postCheckout(
+                    checkoutToRequestDTO(checkout),
+                );
+                setCheckoutData(response);
+                cartService.clearCart();
+                setShowSuccessMessage(true);
+            } finally {
+                setIsLoading(false);
+            }
         }
     };
 
