aaf28a915058a58edc74f6b5e88b18031ce1d9cd
Tomas Mendoza <tm1453766@gmail.com>
2026-06-17 11:34:12 -0300

    style: reacomodo el product detail para que entre en una sola vista

--- client/src/features/product/pages/productDetail/components/productDetailInfo/ProductDetailInfo.tsx
+++ client/src/features/product/pages/productDetail/components/productDetailInfo/ProductDetailInfo.tsx
@@ -1,5 +1,6 @@
 import type { Product } from "../../../../../../shared/types/Product";
 import { useSelectedVariant } from "../../../../hooks/use-select-variant";
+import "./style/DetailInfo.css";
 
 interface ProductDetailInfoProps {
     product: Product;
@@ -18,8 +19,10 @@ export const ProductDetailInfo = ({
         selectedDetailId,
     );
     return (
-        <section>
-            <h1>{product.model}</h1>
+        <section className="product-info">
+            <h1>
+                {product.brand.name} {product.model}
+            </h1>
             <h2>${detail.price.toFixed(2)}</h2>
         </section>
     );
