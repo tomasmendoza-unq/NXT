efb8dc27776a847601d8f6f9cdf4ba1b3ffc4914
Tomas Mendoza <tm1453766@gmail.com>
2026-06-17 15:50:03 -0300

    fix: se genera el estado color y detail en content detail para producto, asi cuando se agrega al carrito se agregue la variante indicada

--- client/src/features/client/pages/Orders.tsx
+++ client/src/features/client/pages/Orders.tsx
@@ -3,14 +3,21 @@ import { useParams } from "react-router-dom";
 import { TableGeneric } from "../../../shared/components/table/TableGeneric";
 import { useGetOrdersClient } from "../hooks/use-get-orders-client";
 import { Detail } from "../../order/components/detail/Detail";
+import { LoadingSpinner } from "../../../shared/components/loadingSpinner/LoadingSpinner";
 
 export const Orders = () => {
     const { status } = useParams<{ status: string }>();
-    const { fetch, orders } = useGetOrdersClient({ status: status ?? "" });
+    const { fetch, orders, isLoading } = useGetOrdersClient({
+        status: status ?? "",
+    });
 
     useEffect(() => {
         fetch();
     }, [status]);
+
+    if (isLoading) {
+        return <LoadingSpinner overlay />;
+    }
     return (
         <section>
             <h1>Ordenes de compra</h1>
--- client/src/features/product/pages/productDetail/components/product/ProductContent.tsx
+++ client/src/features/product/pages/productDetail/components/product/ProductContent.tsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import type { Product } from "../../../../../../shared/types/Product";
 import { FormCart } from "../../../../../cart/components/FormCart";
 import { ProductLayout } from "../../../../components/layout/ProductLayout";
 
 export const ProductContent = ({ product }: { product: Product }) => {
-    const selectedDetailId = product.colors[0]?.details[0]?.id;
+    const [selectedColorId, setSelectedColorId] = useState(
+        () => product.colors[0]?.id,
+    );
+    const [selectedDetailId, setSelectedDetailId] = useState(
+        () => product.colors[0]?.details[0]?.id,
+    );
 
     return (
-        <ProductLayout product={product}>
+        <ProductLayout
+            product={product}
+            selectedColorId={selectedColorId}
+            setSelectedColorId={setSelectedColorId}
+            selectedDetailId={selectedDetailId}
+            setSelectedDetailId={setSelectedDetailId}
+        >
             <section className="product-actions">
-                <FormCart detailSelected={selectedDetailId} />
+                {selectedDetailId ? (
+                    <FormCart detailSelected={selectedDetailId} />
+                ) : (
+                    <p>Por favor, selecciona una combinación válida.</p>
+                )}
             </section>
         </ProductLayout>
     );
