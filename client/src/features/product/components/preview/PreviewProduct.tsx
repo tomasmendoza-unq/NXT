820f01f3cc7bdbab3d11bc5ef2438da52fd4436b
Tomas Mendoza <tm1453766@gmail.com>
2026-06-17 15:53:04 -0300

    fix previewProduct

--- client/src/features/product/components/preview/PreviewProduct.tsx
+++ client/src/features/product/components/preview/PreviewProduct.tsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import { toProductPreview } from "../../adapters/product-request.adapater";
 import type { ProductRequestDTO } from "../../api/types/product-request";
 import { ProductLayout } from "../layout/ProductLayout";
 
 export const PreviewProduct = ({ product }: { product: ProductRequestDTO }) => {
+    const mappedProduct = toProductPreview(product);
+
+    const [selectedColorId, setSelectedColorId] = useState<number>(
+        () => mappedProduct.colors[0]?.id ?? 0,
+    );
+    const [selectedDetailId, setSelectedDetailId] = useState<number>(
+        () => mappedProduct.colors[0]?.details[0]?.id ?? 0,
+    );
+
     return (
         <div className="preview-product">
-            <ProductLayout product={toProductPreview(product)} />
+            <ProductLayout
+                product={mappedProduct}
+                selectedColorId={selectedColorId}
+                setSelectedColorId={setSelectedColorId}
+                selectedDetailId={selectedDetailId}
+                setSelectedDetailId={setSelectedDetailId}
+            />
         </div>
     );
 };
