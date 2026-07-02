5fa1ebe1f6c002a7dbb94ee96d70c0e7f7323b5e
Tomas Mendoza <tm1453766@gmail.com>
2026-07-02 17:09:24 -0300

    wip card news

--- client/src/features/home/pages/Home.tsx
+++ client/src/features/home/pages/Home.tsx
@@ -5,6 +5,8 @@ import { BrandsContainer } from "../../brand/components/BrandsContainer";
 import useGetAllProducts from "../../product/hooks/use-get-all-products";
 import useGetAllBrands from "../../brand/hooks/use-get-all-brands";
 import { LoadingSpinner } from "../../../shared/components/loadingSpinner/LoadingSpinner";
+import { useGetAllNews } from "../../news/hook/use-get-all-news";
+import { CarouselNews } from "../../news/components/carousel/CarrouselNews";
 
 export const Home = () => {
     const { products, fetch, isLoading } = useGetAllProducts();
@@ -14,14 +16,20 @@ export const Home = () => {
         isLoading: isLoadingBrands,
     } = useGetAllBrands();
 
+    const { news, fetchNews, isLoading: isLoadingNews } = useGetAllNews();
+
     useEffect(() => {
         fetch(0, 10);
         fetchBrands();
+        fetchNews();
     }, []);
 
     return (
         <section className="section-container">
-            {(isLoading || isLoadingBrands) && <LoadingSpinner overlay />}
+            {(isLoading || isLoadingBrands || isLoadingNews) && (
+                <LoadingSpinner overlay />
+            )}
+            <CarouselNews news={news.content} />
             <ProductsContainer products={products} />
             <BrandsContainer brands={brands} />
         </section>
